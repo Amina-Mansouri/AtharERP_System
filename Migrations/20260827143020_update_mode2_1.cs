34e32ee2aebff77e7670373749fa5681b4bd8e5b
Amina-Mansouri <aminaalmansore@gmail.com>
2026-08-27 16:31:18 +0200

    3files update model2

---
+++ Migrations/20260827143020_update_mode2_1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtharERP_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_mode2_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
