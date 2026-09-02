27438f0cc93d603de1f57d129025991ce642c5d2
Amina-Mansouri <aminaalmansore@gmail.com>
2026-09-02 13:56:58 +0200

    migration30.8.1

---
+++ Migrations/20260902115607_update-entity 30.8.1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtharERP_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateentity3081 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "identity",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "identity",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                schema: "identity",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
