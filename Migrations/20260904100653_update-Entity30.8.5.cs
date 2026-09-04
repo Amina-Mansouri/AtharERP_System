88e7123037b485e19e7d31d8374c9a8417bf72d9
Amina-Mansouri <aminaalmansore@gmail.com>
2026-09-04 12:08:26 +0200

    update phase4.6

---
+++ Migrations/20260904100653_update-Entity30.8.5.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtharERP_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateEntity3085 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectAssignmentId",
+                table: "ProjectTasks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTasks_ProjectAssignmentId",
+                table: "ProjectTasks",
+                column: "ProjectAssignmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTasks_ProjectAssignments_ProjectAssignmentId",
+                table: "ProjectTasks",
+                column: "ProjectAssignmentId",
+                principalTable: "ProjectAssignments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTasks_ProjectAssignments_ProjectAssignmentId",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectTasks_ProjectAssignmentId",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectAssignmentId",
+                table: "ProjectTasks");
+        }
+    }
+}
