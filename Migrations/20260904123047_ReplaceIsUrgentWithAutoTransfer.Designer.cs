4ed4eb3e4bdb232b89ce73127485910e71dd3e90
Amina-Mansouri <aminaalmansore@gmail.com>
2026-09-04 14:31:04 +0200

    update phase4.13

--- Migrations/20260904123047_ReplaceIsUrgentWithAutoTransfer.Designer.cs
+++ Migrations/20260904123047_ReplaceIsUrgentWithAutoTransfer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AtharERP_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AtharERP_System.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260904123047_ReplaceIsUrgentWithAutoTransfer")]
+    partial class ReplaceIsUrgentWithAutoTransfer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260904123047_ReplaceIsUrgentWithAutoTransfer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtharERP_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceIsUrgentWithAutoTransfer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsUrgent",
+                table: "Projects",
+                newName: "AutoTransferToSite");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AutoTransferToSite",
+                table: "Projects",
+                newName: "IsUrgent");
+        }
+    }
+}
