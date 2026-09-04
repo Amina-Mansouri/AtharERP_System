d56492631759f4cdd0b766e84f996f234e006646
Amina-Mansouri <aminaalmansore@gmail.com>
2026-09-04 13:53:54 +0200

    update phase4.11

---
+++ Migrations/20260904115106_FixStageTaskCascadeDelete.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtharERP_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStageTaskCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTasks_ProjectStages_StageId",
+                table: "ProjectTasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTasks_ProjectStages_StageId",
+                table: "ProjectTasks",
+                column: "StageId",
+                principalTable: "ProjectStages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTasks_ProjectStages_StageId",
+                table: "ProjectTasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTasks_ProjectStages_StageId",
+                table: "ProjectTasks",
+                column: "StageId",
+                principalTable: "ProjectStages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
