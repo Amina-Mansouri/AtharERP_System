620bf6a77dcab1b5928da12223e39000c06e2a15
Amina-Mansouri <aminaalmansore@gmail.com>
2026-09-26 02:05:03 +0200

    update unit1 v2.17

---
+++ Migrations/20260926000408_RenameAssignmentCostTypeToAssignmentType.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtharERP_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameAssignmentCostTypeToAssignmentType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CostType",
+                table: "ProjectAssignments",
+                newName: "AssignmentType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AssignmentType",
+                table: "ProjectAssignments",
+                newName: "CostType");
+        }
+    }
+}
